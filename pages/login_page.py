f9c4bb3177fad656165275d3bd2181a98aca2b91
user <hongseokeom@MacBook-Air-2.local>
2026-02-18 02:35:29 +0900

    login_page

--- pages/login_page.py
+++ pages/login_page.py
@@ -17,12 +17,10 @@ class LoginPage(BasePage):
         self.do_fill(self.password_fld, password)
         self.do_click(self.login_btn)
 
-    # API를 활용한 초고속 로그인 우회
+
     def api_login(self, email, password):
-
-        # 1. BasePage의 do_api_login 메서드 호출
         self.do_api_login(email, password)
-
-        # 2. 로그인 페이지를 거치지 않고 바로 대시보드로 이동
         self.navigate("#/dashboard")
+
+    
 
