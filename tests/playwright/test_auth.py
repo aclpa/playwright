e8a05291ab4cba53e5b09e6e9e59653ca40fb3a6
aclpa <aclpa35@gmail.com>
2026-03-03 14:03:30 +0900

    expect 변경

--- tests/playwright/test_auth.py
+++ tests/playwright/test_auth.py
@@ -2,26 +2,27 @@ from pages.login_page import LoginPage
 from playwright.sync_api import expect
 from pages.dashboard_page import DashboardPage
 import os
+import re
 
 def test_successful_login(page):#TC1 로그인 성공 테스트   
     login_page = LoginPage(page)
     login_page.navigate()
     login_page.login_to_system(os.getenv("ADMIN_EMAIL"), os.getenv("ADMIN_PASS"))
-    expect(page.get_by_text("Dash board").first).to_be_visible(timeout=3000)
+    expect(page).to_have_url(re.compile(r".*/#/dashboard$"))
 
 
 def test_failed_login(page): #TC2 로그인 실패 테스트
     login_page = LoginPage(page)
     login_page.navigate()
     login_page.login_to_system(os.getenv("FAIL_EMAIL"), os.getenv("FAIL_PASS"))
-    expect(page.get_by_text("404")).to_be_visible(timeout=5000)
+    expect(page).to_have_url(re.compile(r".*/#/login$"))
 
 
 def test_api_login(page):# TC3 API로그인 테스트
     login_page = LoginPage(page)
     login_page.api_login(os.getenv("ADMIN_EMAIL"), os.getenv("ADMIN_PASS"))
     login_page.navigate("#/dashboard")      
-    expect(page.get_by_text("DevFlow ERP")).to_be_visible(timeout=10000)
+    expect(page).to_have_url(re.compile(r".*/#/dashboard$"))
 
 
 def test_logout(page): #TC4 로그아웃 테스트
@@ -32,4 +33,4 @@ def test_logout(page): #TC4 로그아웃 테스트
     expect(page.get_by_text("Dash board").first).to_be_visible(timeout=10000)
     dashboard_page.user_menu()
     dashboard_page.logout()
-    expect(page.get_by_text("프로젝트 관리 & 배포 통합 시스템")).to_be_visible(timeout=3000)
+    expect(page).to_have_url(re.compile(r".*/#/login$"))
