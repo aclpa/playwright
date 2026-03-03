61646de781fed9672b82162f79dca1931c61e3ce
aclpa <aclpa35@gmail.com>
2026-03-03 16:05:30 +0900

    timeout 세팅

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -3,7 +3,7 @@ from .base_page import BasePage
 class DashboardPage(BasePage):
 
     def user_menu(self):
-        self.click("//button[.//div[contains(@class, 'q-avatar')]]","avatar")
+        self.click("//button[.//div[contains(@class, 'q-avatar')]]","avatar",timeout=240000)
 
     def profile(self):
         self.click("//div[contains(text(), 'Profile')]", "Profile")
