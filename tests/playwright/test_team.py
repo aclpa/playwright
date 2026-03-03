65b53d4ca57768842ef51c23e493c6ae19f1eccb
aclpa <aclpa35@gmail.com>
2026-03-03 23:57:55 +0900

    팀 api 검증

--- tests/playwright/test_team.py
+++ tests/playwright/test_team.py
@@ -1,9 +1,8 @@
 from pages.team_page import TeamPage
 from pages.login_page import LoginPage
 from playwright.sync_api import expect
-import os
-
 from faker import Faker
+import os
 
 def test_create_team(page, api): #TC8 팀 생성 테스트
     team_page = TeamPage(page)
@@ -15,8 +14,10 @@ def test_create_team(page, api): #TC8 팀 생성 테스트
     with page.expect_response("**/api/v1/teams") as response_info:
         team_page.create_team(team_name)
     response = response_info.value
+    expect(response).to_be_ok()
     new_team_data = response.json()
     team_id = new_team_data.get("id")
-    api.delete(f"api/v1/teams/{team_id}")
+    delete_response = api.delete(f"api/v1/teams/{team_id}")
+    expect(delete_response).to_be_ok()
 
     
