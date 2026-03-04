12ea929b3b47f54580babfcd03fed0bc9bf3cbc6
aclpa <aclpa35@gmail.com>
2026-03-04 20:12:32 +0900

    api 검증 추가

--- tests/playwright/test_sprint.py
+++ tests/playwright/test_sprint.py
@@ -11,12 +11,20 @@ def test_create_sprint(page, api_request): #TC7 스프린트 생성 테스트
     login_page = LoginPage(page)
     fake = Faker()
     sprint_name = fake.lexify(text="????")
+
     login_page.api_login(os.getenv("ADMIN_EMAIL"), os.getenv("ADMIN_PASS"))
+
     sprint_page.navigate("#/sprints")
+
     with page.expect_response("**/api/v1/sprints") as response_info:
         sprint_page.create_sprint(sprint_name)
     response = response_info.value
+
+    expect(page.get_by_text(f"{sprint_name}")).to_be_visible()
+
     sprint_id = response.json().get("id")
-    api_request.delete(f"{api_url}api/v1/sprints/{sprint_id}")
+    delete_api=api_request.delete(f"{api_url}api/v1/sprints/{sprint_id}")
+
+    expect(delete_api).to_be_ok()
     
                     
--- tests/playwright/test_team.py
+++ tests/playwright/test_team.py
@@ -11,14 +11,19 @@ def test_create_team(page, api_request): #TC8 팀 생성 테스트
     api_url = (os.getenv("API_URL"))
     fake = Faker()
     team_name = fake.lexify(text="????")
+
     login_page.api_login(os.getenv("ADMIN_EMAIL"), os.getenv("ADMIN_PASS"))
     team_page.navigate("#/teams")
+
     with page.expect_response("**/api/v1/teams") as response_info:
         team_page.create_team(team_name)
     response = response_info.value
-    team_id = response.json().get("id")
-    api_request.delete(f"{api_url}api/v1/teams/{team_id}")
-        
 
+    expect(page.get_by_text(f"{team_name}")).to_be_visible()
+
+    team_id = response.json().get("id")
+    delete_api=api_request.delete(f"{api_url}api/v1/teams/{team_id}")
+        
+    expect(delete_api).to_be_ok()
     
 
