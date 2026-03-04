246249241b7443a8ada0ef0eec005cbb8cd01331
aclpa <aclpa35@gmail.com>
2026-03-05 00:53:25 +0900

    디버깅 메세지 추가

--- tests/playwright/test_team.py
+++ tests/playwright/test_team.py
@@ -13,6 +13,7 @@ def test_create_team(page, api_request): #TC9 팀 생성 테스트
 
     login_page.api_login(os.getenv("ADMIN_EMAIL"), os.getenv("ADMIN_PASS"))
     team_page.navigate("#/teams")
+    page.on("request", lambda req: print(f"{req.method} {req.url}"))
 
     with page.expect_response("**/api/v1/teams") as response_info:
         team_page.create_team(team_name)
