10e1f0cfe683822d5ced0255a1b3e8ba50e227b4
aclpa <aclpa35@gmail.com>
2026-03-09 16:56:46 +0900

    locator 수정

--- pages/project_page.py
+++ pages/project_page.py
@@ -4,8 +4,8 @@ class ProjectPage(BasePage):
 
     def create_project(self, project_name, project_key,test_team):
         self.click('button:has-text("project")', "Project")
-        self.fill('area-label:has-text("프로젝트 이름 *")',"프로젝트 이름",project_name)
-        self.fill('area-label:has-text("프로젝트 키 *")',"프로젝트 키",project_key)
+        self.fill('label:has-text("프로젝트 이름 *")',"프로젝트 이름",project_name)
+        self.fill('label:has-text("프로젝트 키 *")',"프로젝트 키",project_key)
         self.click("input[role=combobox][aria-label='Team *']", "Team")
         self.click(f'.q-virtual-scroll__content >> text="{test_team}"', test_team)
         self.click('button:has-text("Create")', "Create")
